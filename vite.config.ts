32bc764c474449099acb76e36a5c0722afe8afc1
Rafael Pereira <rafael.pereira.sjbv@gmail.com>
2023-06-26 18:16:58 -0300

    Adding support for SVGR

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
     port: 3000,
   },
